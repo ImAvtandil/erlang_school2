76205cb903653be4d16776cfeb1911205ed95c73
sanr <oleksandr.lishchynskyy@ria.com>
2023-08-29 16:48:17 +0300

    All tasks are ready

---
+++ lesson3_task2.erl
@@ -0,0 +1,24 @@
+-module(lesson3_task2).
+-export([words/1, reverse/1]).
+
+
+
+words(String)->
+    reverse(words(String, <<>>, [])).
+
+
+words(<<S/utf8, 32, T/binary>>, AccBin, Acc)->
+    words(T, <<>>, [<<AccBin/binary, S>> | Acc]);
+words(<<S/utf8, T/binary>>, AccBin, Acc)->
+    words(T, <<AccBin/binary, S>>, Acc);
+words(<<>>, AccBin, Acc)->
+    [AccBin | Acc].
+
+
+reverse(List)->
+    reverse(List,[]).
+
+reverse([H|T], Acc)->
+    reverse(T, [H|Acc]);
+reverse([], Acc)->
+    Acc.
